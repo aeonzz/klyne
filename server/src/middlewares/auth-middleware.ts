2453756e268a07019ad00dc88e6896b4f7aea1ae
aeonz <christiancaneos1@gmail.com>
2025-02-15 12:25:37 +0800

    feat: change better auth session headers format

--- server/src/middlewares/auth-middleware.ts
+++ server/src/middlewares/auth-middleware.ts
@@ -1,30 +1,12 @@
 import type { Request, Response, NextFunction } from "express";
 import { auth } from "../lib/auth";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { fromNodeHeaders } from "better-auth/node";
 
 const authorize = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    console.log(req.headers.cookie)
-    const headers: any = {
-      get: (key: string) => req.headers[key.toLowerCase()],
-      set: (key: string, value: string) => {
-        req.headers[key.toLowerCase()] = value;
-      },
-      getSetCookie: () => [],
-      append: (key: string, value: string) => {
-        const currentValue = req.headers[key.toLowerCase()];
-        if (Array.isArray(currentValue)) {
-          currentValue.push(value);
-        } else if (currentValue) {
-          req.headers[key.toLowerCase()] = [currentValue, value];
-        } else {
-          req.headers[key.toLowerCase()] = value;
-        }
-      },
-    };
-
     const session = await auth.api.getSession({
-      headers,
+      headers: fromNodeHeaders(req.headers),
     });
 
     if (!session) {
