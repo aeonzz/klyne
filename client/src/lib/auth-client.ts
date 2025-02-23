78629db2a61ac195829586095c56e59aacfca3bd
aeonz <christiancaneos1@gmail.com>
2025-02-23 20:30:45 +0800

    test: session bug

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -2,6 +2,9 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_API_URL,
+  fetchOptions: {
+    credentials: "include",
+  },
 });
 
 export const { signIn, useSession, signUp, getSession, signOut } = authClient;
